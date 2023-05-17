bbfb9bd01f44aaaca8d8940bffed1d52617f4f40
slalom92 <slalom92@chunjae.co.kr>
2023-05-17 15:01:53 +0900

    PC상단 네비게이션 완성

--- js/common.js
+++ js/common.js
@@ -21,17 +21,43 @@ var init = {
             return false;
         }        
         $('.headerGnb>li>a').bind('mouseover',function(){
-            $('.subGnbLayer').stop().slideUp();
+            $('.headerGnb>li>a').removeClass('is_active');
+            $(this).addClass('is_active');
+            if($(this).next().css('display')=='block'){
+                return false;
+            }
+            $('.subGnbLayer').slideUp(200);
             $(this).next().stop().slideDown();
         });
         $(document).bind('mouseover', function(e) {
             var $clicked = $(e.target);
-            if (!$clicked.parents().hasClass("headerGnb"))
-            $('.subGnbLayer').stop().slideUp();
+            if (!$clicked.parents().hasClass("headerGnb")){
+                $('.headerGnb>li>a').removeClass('is_active');
+                $('.subGnbLayer').slideUp(200);
+            }
         });
+    },
+    gnbScroll : function(){
+        var winTop = $(window).scrollTop();
+        if(winTop>100){
+            $('.headerNav').css({
+                'position':'fixed',
+                'top':0,
+                'left':0
+            });
+        }else{
+            $('.headerNav').css({
+                'position':'relative'
+            });
+        }
     }
 }
+// 문서 호출 시
 $(document).ready(function(){
     init.topRightNav(); // 우측상단 네비
     init.gnbNav(); // 상단 네비
+    init.gnbScroll(); // 상단 네비 고정
+    $(window).scroll(function(){
+        init.gnbScroll();
+    })
 });
