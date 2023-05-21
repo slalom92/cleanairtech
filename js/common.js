79cc582a48808f4de158712366983dcd1cb71144
Shinzyoung <shinz520@naver.com>
2023-05-21 09:07:29 +0900

    모바일 경우 스크립트 분기

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
-var init = {
-    winWidth : $(window).width(),
+ var winWidthPx = $(window).width();
+ var init = {
     topRightNav : function(){
         if(!$('.showNav').length > 0){
             return false;
@@ -22,55 +22,61 @@ var init = {
         if(!$('#gnb').length > 0){
             return false;
         }
-        $('.headerGnb>li>a').on('mouseover',function(){
-            $('.headerGnb>li>a').removeClass('is_active');
-            $(this).addClass('is_active');
-            if($(this).next().css('display')=='block'){
-                return false;
-            }
-            $('.subGnbLayer').slideUp(200);
-            $(this).next().stop().slideDown();
-        });
-        $(document).bind('mouseover', function(e) {
-            var $clicked = $(e.target);
-            if (!$clicked.parents().hasClass("headerGnb")){
+        if(winWidthPx>1280){
+            $('.headerGnb>li>a').on('mouseover',function(){
                 $('.headerGnb>li>a').removeClass('is_active');
+                $(this).addClass('is_active');
+                if($(this).next().css('display')=='block'){
+                    return false;
+                }
                 $('.subGnbLayer').slideUp(200);
-            }
-        });
+                $(this).next().stop().slideDown();
+            });
+            $(document).bind('mouseover', function(e) {
+                var $clicked = $(e.target);
+                if (!$clicked.parents().hasClass("headerGnb")){
+                    $('.headerGnb>li>a').removeClass('is_active');
+                    $('.subGnbLayer').slideUp(200);
+                }
+            });
+        }else{
+            $('.headerGnb>li>a').off('mouseover');
+            $(document).off('mouseover');
+        }
     },
     gnbScroll : function(){
         var winTop = $(window).scrollTop();
-        if(winTop>100){
-            $('.headerNav').css({
-                'position':'fixed',
-                'top':0,
-                'left':0
-            });
+        if(winWidthPx>1280){
+            if(winTop>100){
+                $('.headerNav').css({
+                    'position':'fixed',
+                    'top':0,
+                    'left':0
+                });
+            }else{
+                $('.headerNav').css({
+                    'position':'relative'
+                });
+            }
         }else{
             $('.headerNav').css({
-                'position':'relative'
+                'position':'absolute'
             });
         }
     }
 }
 // 문서 호출 시
 $(document).ready(function(){
-    var winHeightPx = $(window).width();
     //PC 사이즈
-    if(winHeightPx>1280){
-        init.topRightNav(); // 우측상단 네비
-        init.gnbNav(); // 상단 네비
-        init.gnbScroll(); // 상단 네비 고정
-    }
-    $(window).resize(function(){
-        winHeightPx = $(window).width();
-        init.winWidth = winHeightPx;
-        console.log(init.winWidth);
-        if(winHeightPx>1280){
-            init.topRightNav(); // 우측상단 네비
-            init.gnbNav(); // 상단 네비
-            init.gnbScroll(); // 상단 네비 고정
-        }
-    });
+    winWidthPx = $(window).width();  
+    init.topRightNav(); // 우측상단 네비
+    init.gnbNav(); // 상단 네비
+    init.gnbScroll(); // 상단 네비 고정
+});
+// 리사이즈 시 호출
+$(window).on('resize',function(){
+    winWidthPx = $(window).width();
+    init.topRightNav(); // 우측상단 네비
+    init.gnbNav(); // 상단 네비
+    init.gnbScroll(); // 상단 네비 고정
 });
